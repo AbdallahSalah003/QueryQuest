981e528dee941acb0917c2e4f4103e19d089843c
AbdallahSalah003 <abdallahsalahsalem9@gmail.com>
2024-05-03 23:42:32 +0300

    Intial commit

---
+++ src/main/java/dev/engine/queryquestapi/QueryQuestApiApplication.java
@@ -0,0 +1,13 @@
+package dev.engine.queryquestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QueryQuestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QueryQuestApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/engine/queryquestapi/QueryQuestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.engine.queryquestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QueryQuestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
