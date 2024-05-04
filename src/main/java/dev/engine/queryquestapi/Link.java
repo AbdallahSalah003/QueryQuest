9cfca129f430752c75f3303147721912ff17c18e
AbdallahSalah003 <abdallahsalahsalem9@gmail.com>
2024-05-04 05:23:10 +0300

    Add Validation to Link record

--- src/main/java/dev/engine/queryquestapi/Link.java
+++ src/main/java/dev/engine/queryquestapi/Link.java
@@ -1,9 +1,15 @@
 package dev.engine.queryquestapi;
 
+import jakarta.validation.constraints.NotEmpty;
+
 public record Link(
+        @NotEmpty
         String url,
+        @NotEmpty
         String content,
+        @NotEmpty
         String description,
+        @NotEmpty
         String title
 ) {
 }
